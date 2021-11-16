d8922324f208c8e1170e5ba41095f665200769b1
Richard Patsch <richard.patsch@technikum-wien.at>
2021-11-16 15:01:42 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import math
+import random
+
+
+def generate_random_number(max):
+    return random.randrange(max)
+
+def check_number(solution, max_number, modulo, residual_class):
+    for i in range(max_number):
+        if i % modulo == residual_class:
+            #print(i)
+            if i == solution:
+                return i
+    return False
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    max_number = int(math.pow(10, 4))
+    random_number = generate_random_number(max_number)
+
+    print('max: ', max_number)
+    print('random: ', random_number)
+
+    modulo = 3
+
+    for i in range(modulo):
+        print('mod ' + str(modulo) + ' / ' + str(i))
+        result = check_number(random_number, max_number, modulo, i)
+        if result:
+            print('SOLUTION: ' + str(result))
+            break
+        else:
+            print('try next')
+
+
+
